bac2e158a2038e8e5e9afb0b63ebfaff2a0ad734
Pavel <146548629+Pavelko01@users.noreply.github.com>
2023-10-07 15:34:05 +0300

    Update Ping_pong.py
    v1.1

--- Ping_pong.py
+++ Ping_pong.py
@@ -4,15 +4,24 @@ from random import *
 
 
 w_w = 1200
-w_h = 900
+w_h = 750
 
 
 #
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render("Первый игрок проиграл!", True,(180,0,0))
+lose2 = font1.render("Второй игрок проиграл!", True,(180,0,0))
 window = display.set_mode((w_w,w_h))
 display.set_caption("Пинг-понг")
 clock = time.Clock()
 fps = 60
 bacgroop = transform.scale(image.load("fon.jpg"), (w_w,w_h))
+
+sp_x = 3
+sp_y = 3
+
+
 
 
 
@@ -52,7 +61,7 @@ Playerok1 = Player("igrok.png",50,300,20, 30,150)
 
 Playerok2 = Player("igrok.png",1100,300,20, 30,150)
 
-mich = Gamespite("bals.png", 600, 450, 20,80,80 )
+mich = Gamespite("pngwing.com (1).png", 600, 450, 20,80,80 )
 
 
 
@@ -71,7 +80,20 @@ while game:
         Playerok2.reset()
         Playerok2.update2()
         mich.reset()
-
+        mich.rect.x += sp_x
+        mich.rect.y += sp_y
+        if sprite.collide_rect(Playerok1, mich) or sprite.collide_rect(Playerok2, mich):
+            sp_x *= -1
+        if mich.rect.y > w_h - 50 or mich.rect.y < 0:
+            sp_y *= -1
+        
+        if mich.rect.x < 0:
+            finish = True
+            window.blit(lose1, (400, 325))
+        if mich.rect.x > 1100:
+            finish = True
+            window.blit(lose2, (600, 325))
+            
 
 
         display.update()
